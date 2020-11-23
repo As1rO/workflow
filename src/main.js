d0fd86f2a7d9cd84fa99a914ee0f410a65f29474
As1rO <olivier.denis@hotmail.be>
2020-11-23 12:47:50 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './scss/style.scss';
+
+/**
+ *  if you want to use bootstrap styles
+ *  copy @import '~bootstrap/scss/bootstrap'; in style.scss
+ */
+
+/**
+ * import multiple js files
+ *
+ * import { nameFunction } from "./name.js"
+ */
+
+/**
+ * if you want to use bootstrap
+ *
+ * import 'bootstrap';
+ */
+
+/**
+  * if you want to use jquery
+  *
+  * import $ from 'jquery';
+  */
